dac073bbd0e3022bc0f94825b7152b614c6b4874
mariukz3 <mariukz3@yahoo.com>
2014-04-06 02:52:53 +0300

    Pridetas algoritmas papildomai tekstinio failo statistikai - raidziu tipo ir ju pasikartojimo tekste - kaupti, naudojant metoda Counter

--- antrasis_atsiskaitymas.py
+++ antrasis_atsiskaitymas.py
@@ -3,3 +3,10 @@ def SutvarkytiFaila(SkaitomasFailas, RezultatuFailas):
     num_lines = 0
     num_words = 0
     num_chars = 0
+
+    with open(str(SkaitomasFailas)) as f:
+        result.write("Raidziu yra:")
+        result.write(str(Counter(
+            letter for line in f
+            for letter in line.lower()
+            if letter in ascii_lowercase)))
