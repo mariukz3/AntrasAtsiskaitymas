680313e7d900772dfebc63d24aabaa5a48b095aa
mariukz3 <mariukz3@yahoo.com>
2014-04-06 23:07:07 +0300

    Pakeisti funkciju kintamuju pavadinimai is CamelCase i kintamuosius_su_pabraukimais. Pakeisti komentarai: po groteliu padeti tarpai PEP8 standartui atitikti.

--- antrasis_atsiskaitymas.py
+++ antrasis_atsiskaitymas.py
@@ -2,50 +2,50 @@ from string import ascii_lowercase
 from collections import Counter
 import glob
 
-#Globalus kintamieji bendrai statistikai kaupti
+# Globalus kintamieji bendrai statistikai kaupti
 
 GLOBLINES = 0
 GLOBWORDS = 0
 GLOBCHARS = 0
 
-#Funkcija vieno failo statistikoms kaupti
+# Funkcija vieno failo statistikoms kaupti
 
 
-def SurinktiFailoStatistikas(SkaitomasFailas, RezultatuFailas):
+def SurinktiFailoStatistikas(skaitomas_failas, rezultatu_failas):
 
-#Statistiku kintamieji
+# Statistiku kintamieji
 
     num_lines = 0
     num_words = 0
     num_chars = 0
 
-#Rasomo failo kintamojo sukurimas
+# Rasomo failo kintamojo sukurimas
 
-    result = open(RezultatuFailas, 'w')
+    result = open(rezultatu_failas, 'w')
 
-#Raidziu tipo ir ju pasikartojimo daznumo algoritmas
+# Raidziu tipo ir ju pasikartojimo daznumo algoritmas
 
-    with open(str(SkaitomasFailas)) as f:
+    with open(str(skaitomas_failas)) as f:
         result.write("Raidziu yra:")
 
-#Raidziu statistikos isvedimas
+# Raidziu statistikos isvedimas
 
         result.write(str(Counter(
             letter for line in f
             for letter in line.lower()
             if letter in ascii_lowercase)))
 
-#Eiluciu skaiciaus, zodziu skaiciaus,
-#simboliu skaiciaus statistiku apdorojimas
+# Eiluciu skaiciaus, zodziu skaiciaus,
+# simboliu skaiciaus statistiku apdorojimas
 
-    with open(str(SkaitomasFailas)) as f:
+    with open(str(skaitomas_failas)) as f:
         for line in f:
             words = line.split()
             num_lines += 1
             num_words += len(words)
             num_chars += len(line)
 
-#...ju irasymas i atskira faila ir jo uzdarymas
+# ...ju irasymas i atskira faila ir jo uzdarymas
 
     result.write('\nEiluciu skaicius:')
     result.write(str(num_lines))
@@ -55,7 +55,7 @@ def SurinktiFailoStatistikas(SkaitomasFailas, RezultatuFailas):
     result.write(str(num_chars))
     result.close()
 
-#Globalios statistikos kintamuju apdorojimas
+# Globalios statistikos kintamuju apdorojimas
 
     global GLOBLINES
     GLOBLINES = GLOBLINES + num_lines
@@ -66,34 +66,34 @@ def SurinktiFailoStatistikas(SkaitomasFailas, RezultatuFailas):
 
     result.close()
 
-#Funkcija sujungti visiems failu tekstams i viena
+# Funkcija sujungti visiems failu tekstams i viena
 
 
-def SujungtiFailus(RezultatuFailas):
+def SujungtiFailus(rezultatu_failas):
 
-#Kintamasis atskirti tik fialus, prasidedancius
-#su Read + simbolis kiekvieno identifikacijai
+# Kintamasis atskirti tik fialus, prasidedancius
+# su Read + simbolis kiekvieno identifikacijai
 
     read_files = glob.glob('Read*.txt')
 
-#Bendro teksto rasymo algoritmas
+# Bendro teksto rasymo algoritmas
 
-    with open(str(RezultatuFailas), "wb") as outfile:
+    with open(str(rezultatu_failas), "wb") as outfile:
         for f in read_files:
             with open(f, "rb") as infile:
                 outfile.write(infile.read())
 
-#Funkcija ivesti bendrai visu failu statistikai
-#i atskira faila (globalus kintamieji)
+# Funkcija ivesti bendrai visu failu statistikai
+# i atskira faila (globalus kintamieji)
 
 
-def SurinktiFailuStatistikas(RezultatuFailas):
+def SurinktiFailuStatistikas(rezultatu_failas):
 
-#Iseities failo kintamasis result
+# Iseities failo kintamasis result
 
-    result = open(RezultatuFailas, 'w')
+    result = open(rezultatu_failas, 'w')
 
-#Irasymo ir uzdarymo metodai
+# Irasymo ir uzdarymo metodai
 
     result.write('\nBendras failu eiluciu skaicius:')
     result.write(str(GLOBLINES))
@@ -102,12 +102,3 @@ def SurinktiFailuStatistikas(RezultatuFailas):
     result.write('\nBendras failu simboliu skaicius:')
     result.write(str(GLOBCHARS))
     result.close()
-
-#Statistiku isvedimas
-
-SurinktiFailoStatistikas('Read1.txt', 'Statistika1.txt')
-SurinktiFailoStatistikas('Read2.txt', 'Statistika2.txt')
-SurinktiFailoStatistikas('Read3.txt', 'Statistika3.txt')
-SujungtiFailus('BendrasTekstas.txt')
-SurinktiFailuStatistikas('VisuFailuBendraStatistika')
-SurinktiFailoStatistikas('BendrasTekstas.txt', 'BendraStatistika.txt')
